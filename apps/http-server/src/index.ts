ac559f0dbaa82e3c8dfee5fbb5f28de0261a6b9a
Ankit Kumar Singh <ankitkumarsingh221b072@gmail.com>
2025-08-13 22:57:15 +0530

    finally

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -2,7 +2,7 @@ import express from "express"
 const app = express();
 
 app.get("/",(req,res)=>{
-    res.send("Hola Amigo... Kaise ho thik ho ?")
+    res.send("Hola Amigo... Kaise ho thik ho...Part2 ?")
 })
 
 app.listen(3001);
